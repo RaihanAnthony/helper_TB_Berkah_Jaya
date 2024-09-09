859f0384e8e2044483d7ca4bc99a0e824de4a9b2
RaihanAnthony <raihanmalay14@gmail.com>
2024-09-09 12:31:41 +0700

    add header set content type

--- response.go
+++ response.go
@@ -6,6 +6,7 @@ import (
 )
 
 func Response(w http.ResponseWriter, payload interface{}, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
 	response, _ := json.Marshal(payload)
 	w.WriteHeader(statusCode)
 	w.Write(response)
